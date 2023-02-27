d6d61c20eda7ce3082440ffea8e2da52259572b1
ohsand <osand.business@gmail.com>
2023-02-27 16:29:22 +0100

    add router and create components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
+
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components';
+import './App.css';
 
 const App = () => {
   return (
     <div className='app'>
-        <div className='nav'>
-
+        <div className='navbar'>
+            <Navbar />
         </div>
         <div className='main'>
-
+            <Layout>
+                <div className='routes'>
+                    <Routes>
+                        <Route exact path='/' element={<Homepage />} />
+                        <Route exact path='/exchanges' element={<Exchanges />} />
+                        <Route exact path='/cryptocurrencies' element={<Cryptocurrencies />} />
+                        <Route exact path='/crypto/:coinId' element={<CryptoDetails />} />
+                        <Route exact path='/news' element={<News />} />
+                    </Routes>
+                </div>
+            </Layout>
+            <div className='footer'>
+                <Typography.Title  level={5} style={{ color: 'white', textAlign: 'center' }}>
+                    CryptoVerse <br />
+                    All rights reserved
+                </Typography.Title>
+                <Space>
+                    <Link to='/'>Home</Link>
+                    <Link to='/exchanges'>Exchanges</Link>
+                    <Link to='/news'>News</Link>
+                </Space>
+            </div>
         </div>
     </div>
   );
